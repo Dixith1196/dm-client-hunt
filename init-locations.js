f6685b2ae063a5a9282bf9b62c369810b11c0b2c
Deekshith Maram <s538365@nwmissouri.edu>
2021-01-27 23:44:58 -0600

    comments in main.js

---
+++ init-locations.js
@@ -0,0 +1,13 @@
+export default [
+    {
+        Name: 'Horizons Apartments',
+        Latitude: 40.383849,
+        Longitude: -94.887384
+    },
+    {
+        Name: 'Village O Apartments',
+        Latitude: 40.360970,
+        Longitude: -94.888242
+    }
+
+];
